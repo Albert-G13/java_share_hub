0245250ac8368cb269d1838bd4055446b69ab1bc
Albert G. <a.golovanov1323@gmail.com>
2026-02-07 18:56:56 +0600

    Update 007-insert-users.sql

--- src/main/resources/db/changelog/initial/007-insert-users.sql
+++ src/main/resources/db/changelog/initial/007-insert-users.sql
@@ -5,6 +5,6 @@ WHERE NOT EXISTS (SELECT 1 FROM users_table WHERE email = 'user@test.com');
 INSERT INTO users_table (email, password, name, role_id, enabled)
 SELECT 'user@test2.com', '$2a$10$vap5iYlFWCw0BNpVfQsf8.U8OSf9VuuW1S6d4Kg7jOqbbu98NYkdC', 'User2',
        (SELECT id FROM roles_table WHERE role = 'USER'), true
-    WHERE NOT EXISTS (SELECT 1 FROM users_table WHERE email = 'user@test.com');
+    WHERE NOT EXISTS (SELECT 1 FROM users_table WHERE email = 'user@test2.com');
 --пароль Qwe11
 --$2a$10$vap5iYlFWCw0BNpVfQsf8.U8OSf9VuuW1S6d4Kg7jOqbbu98NYkdC
